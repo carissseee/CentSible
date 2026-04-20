73d77fda48c5635e4cb96c52531c94c4e8357d93
carissseee <carisseauditor@gmail.com>
2026-04-21 07:33:35 +0800

    Setup the base project structures including Models, Database, Logic, and Form layers

---
+++ CentSible/CentSible.App/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CentSible.Models
+{
+    public class Account
+    {
+        public int AccountID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public Date CreatedAt { get; set; }
+        public int CurrentStreak { get; set; }
+        public int HighestStreak { get; set; }
+        public Date LastLogin { get; set; }
+    }
+}
