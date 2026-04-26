d90f1f2a469a36210e340dc195d69a01a5651526
carissseee <carisseauditor@gmail.com>
2026-04-26 16:28:13 +0800

    Adding files for each project layers

---
+++ CentSible - Copy/Logic/AccountLogic.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CentSible.Models;
+using CentSible.Database;
+using System.Drawing;
+
+namespace CentSible.Logic
+{
+    public class AccountLogic
+    {
+        private AccountDB db = new AccountDB();
+
+        public class StreakMetrics
+        {
+            public int CurrentStreak { get; set; }
+            public int LongestStreak { get; set; }
+            public int MaxGoal { get; set; } = 30;
+            public string StreakText { get; set; }
+            public Color StatusColor { get; set; }
+            public bool IsMaster { get; set; }
+        }
+
+        public Account GetAuthenticatedUser(string username, string password)
+        {
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+                throw new Exception("Please enter both username and password.");
+
+            Account user = db.GetAccount(username, password);
+            if (user != null)
+            {
+                int newStreak = CalculateStreak(user.LastLoginDate, user.LoginStreak);
+                int newLongest = (newStreak > user.LongestStreak) ? newStreak : user.LongestStreak;
+                user.LoginStreak = newStreak;
+                user.LongestStreak = newLongest;
+                db.UpdateStreakAndDate(user.AccountID, newStreak, newLongest);
+                return user;
+            }
+            return null;
+        }
+
+        public void RegisterUser(string username, string password, string confirm)
+        {
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+                throw new Exception("All fields are required.");
+            if (password != confirm) throw new Exception("Passwords do not match.");
+            db.RegisterAccount(username, password);
+        }
+
+        public StreakMetrics GetStreakMetrics(Account user)
+        {
+            if (user == null) return new StreakMetrics();
+            int max = 30;
+            bool mastered = user.LongestStreak >= max;
+            return new StreakMetrics
+            {
+                CurrentStreak = user.LoginStreak,
+                LongestStreak = user.LongestStreak,
+                IsMaster = mastered,
+                StatusColor = mastered ? Color.Gold : Color.OrangeRed,
+                StreakText = mastered ? $"MONTHLY MASTER: {user.LongestStreak} Days" : $"Record: {user.LongestStreak}/{max} to Mastery"
+            };
+        }
+
+        private int CalculateStreak(DateTime? lastLogin, int currentStreak)
+        {
+            if (lastLogin == null) return 1;
+            double days = (DateTime.Today - lastLogin.Value.Date).TotalDays;
+            if (days == 1) return currentStreak + 1;
+            if (days == 0) return currentStreak;
+            return 1;
+        }
+    }
+}
---
+++ CentSible - Copy/Views/SummaryForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using CentSible.Models;
+
+namespace CentSible.Views
+{
+    public partial class SummaryForm : Form
+    {
+        private Form _home;
+        private Account _user;
+        private bool _isNavigating = false;
+
+        public SummaryForm() { InitializeComponent(); }
+
+        public SummaryForm(Form home, Account user)
+        {
+            InitializeComponent();
+            _home = home;
+            _user = user;
+        }
+
+        private void SummaryForm_Load(object sender, EventArgs e)
+        {
+            if (_user == null) return;
+
+            btnNavSum.BackColor = Color.FromArgb(212, 236, 204);
+            btnNavSum.ForeColor = Color.Black;
+        }
+
+        private void Switch(Form next) { _isNavigating = true; next.Show(); this.Close(); }
+        private void btnNavHome_Click(object sender, EventArgs e) { _isNavigating = true; _home.Show(); this.Close(); }
+        private void btnNavGoal_Click(object sender, EventArgs e) => Switch(new GoalForm(_home, _user));
+        private void btnNavTrans_Click(object sender, EventArgs e) => Switch(new TransactionForm(_home, _user));
+        private void btnNavSum_Click(object sender, EventArgs e) { }
+        private void btnNavPred_Click(object sender, EventArgs e) => Switch(new PredictionForm(_home, _user));
+        private void btnNavLogout_Click(object sender, EventArgs e)
+        {
+            new LoginForms().Show(); 
+            this.Dispose();
+        }
+
+        private void SummaryForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!_isNavigating && e.CloseReason == CloseReason.UserClosing)
+            {
+                Application.Exit();
+            }
+        }
+
+       
+    }
+}
