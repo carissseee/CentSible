249aa81a49295526bff3138e0233b80c8962709e
JaredAningalan <jjaredaningalan@gmail.com>
2026-05-05 20:41:41 +0800

    Updated HomeForm and GoalForm

---
+++ CentSible/Models/GoalDisplayMetrics.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace CentSible.Models
+{
+    public class GoalDisplayMetrics
+    {
+        public int TotalPercent { get; set; }
+        public int MilestoneStep { get; set; }
+        public string StatusNote { get; set; }
+        public Color ThemeColor { get; set; }
+        public string DaysText { get; set; }
+        public string AmountText { get; set; }
+    }
+}
